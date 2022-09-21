7fd64a8e3bc24e16533f34f2ec20a3741dca0dbc
20ftt1795@student.pb.edu.bn <20ftt1795@student.pb.edu.bn>
2022-09-21 15:46:29 +0800

    New Auth App

---
+++ app/src/main/java/com/example/authapp/User.java
@@ -0,0 +1,16 @@
+package com.example.authapp;
+
+public class User {
+
+    public String fullname, age, email;
+
+    public User(){
+
+    }
+
+    public User(String fullname, String age, String email){
+        this.fullname = fullname;
+        this.age = age;
+        this.email = email;
+    }
+}
